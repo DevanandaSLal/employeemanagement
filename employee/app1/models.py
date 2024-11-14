311967c96508e50b95e9222b25a37349a4ce2295
DevanandaSLal <devanandaslal2000@gmail.com>
2024-11-14 14:49:31 +0530

    added

---
+++ employee/app1/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    ename=models.CharField(max_length=20)
+    age=models.IntegerField()
+    address=models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
+
+    def __str__(self):
+        return self.ename
